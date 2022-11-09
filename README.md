1ba6e5dfe874ee1554ebdb1139ef79ab5a32ee32
Holly Robertson <holly.robertson@slpc437.slcu.cam.ac.uk>
2022-11-09 19:01:45 +0000

    Edits to README, comments and logfile formatting for clarity.

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # RootingAndVariance
-A quick program that midpoint roots trees with dendropy then calculates and reports their root-to-tip variance. 
+A program that midpoint roots trees then calculates and reports their root-to-tip variance. 
 
-Dependencies: phyx (https://github.com/FePhyFoFum/phyx) and dendropy (https://dendropy.org/primer/index.html).
+
+###Usage: 
+
+```
+python3 RootingAndVariance.py folder_of_genes outfile_prefix
+```
+
+For example, using the ExampleGenes folder to create the outputs example.csv and example.log as included here, the command was:
+
+```
+python3 RootingAndVariance.py ExampleGenes example
+```
+
+###Details
+The output file \[outfile\_prefix\].csv lists the name of each file in the gene column and the corresponding root-to-tip variance, as calculated by the [phyx](https://github.com/FePhyFoFum/phyx) tool pxlstr, in the root\_tip\_var column. 
+
+Phyx will also produce a log called phyx.logfile, and the program generates a file called temp.tre that can be deleted after running. 
+
+###Dependencies
+This program uses [DendroPy](https://dendropy.org/primer/index.html) to midpoint root unrooted trees and [phyx](https://github.com/FePhyFoFum/phyx) to calculate root-to-tip variance.
--- RootingAndVariance.py
+++ RootingAndVariance.py
@@ -7,16 +7,20 @@ import os
 This program is intended to take as input a folder of gene trees, midpoint root each of them, and then output a table containing each gene tree name with its root-to-tip variance (calculated using pxlstr in phyx). 
 """
 
-
+# Checking inputs and getting the gene list - we expect a gene folder name and 
+# a prefix for the output files. 
 if len(sys.argv) == 3:
     folder_name = sys.argv[1]
     out_prefix = sys.argv[2]
 else:
-    print("\nUsage: " + sys.argv[0] + " folder_of_genes outfile_prefix\n")
+    print("\nUsage: python3 " + sys.argv[0] + " folder_of_genes outfile_prefix\n")
     sys.exit()
 
 gene_tree_list = os.listdir(folder_name)
 
+
+# When we write out to the .csv file, we want to include headers for the columns
+# on the first line. 
 firstTime = True
 
 for gene in gene_tree_list:
@@ -24,7 +28,7 @@ for gene in gene_tree_list:
         for line in f:
             tree_str = line
 
-        # First we read the tree
+        # First we read the tree.
         tree = dendropy.Tree.get(
                 data = tree_str,
                 schema = "newick",
@@ -33,18 +37,19 @@ for gene in gene_tree_list:
         # Then remove outgroups
         #tree.prune_taxa_with_labels([OUTGROUPS])
 
-        # Then we midpoint root
+        # Then we midpoint root.
         tree.reroot_at_midpoint()
 
-        # Then we get the rooted tree and chop off the [&R] bit
+        # Then we get the rooted tree and chop off the "[&R] " at the start.
         rooted_tree = tree.as_string(schema='newick')
         rooted_tree = rooted_tree[5:]
 
-        # Now write the rooted tree out to a new directory
+        # Now write the rooted tree out to a temporary file, as phyx takes a 
+        # file as input.
         with open("temp.tre", 'w') as f:
             f.write(rooted_tree)
 
-        # Now use phyx to get the root to tip variance
+        # Getting root-to-tip variance with phyx. 
         cmd = ["pxlstr", "-v", "-t", "temp.tre"]
 
         var_process = subprocess.run(cmd, text = True, capture_output = True)
@@ -52,28 +57,20 @@ for gene in gene_tree_list:
         
         root_tip_var = root_tip_var.strip()
         
-        # Writing the variance out to the file with the gene names
+        # Making the .csv file... 
         if firstTime:
             with open(out_prefix + ".csv", 'w') as f:
-                f.write("gene,root_tip_var\n")
+                f.write("gene,root_tip_var\n") # Column headers! 
                 f.write(gene + "," + root_tip_var + "\n")
         else:
             with open(out_prefix + ".csv", "a") as f:
                 f.write(gene + "," + root_tip_var + "\n")
 
-        if firstTime:
-            with open(out_prefix + ".log", 'w') as f:
-                f.write("gene name: \n" + gene + "\n")
-                f.write("original tree: \n" + tree_str + "\n")
-                f.write("rooted tree: \n" + rooted_tree + "\n")
-                f.write("getting root to tip variance: " + str(var_process) + "\n")
-
-        else:
-            with open(out_prefix + ".log", 'a') as f:
-                f.write("gene name: \n" + gene + "\n")
-                f.write("original tree: \n" + tree_str + "\n")
-                f.write("rooted tree: \n" + rooted_tree + "\n")
-                f.write("getting root to tip variance: \n" + str(var_process) + "\n")
-
+        # ...and the log. 
+        with open(out_prefix + ".log", 'a') as f:
+            f.write("gene name: \n" + gene + "\n")
+            f.write("original tree: \n" + tree_str)
+            f.write("rooted tree: \n" + rooted_tree)
+            f.write("getting root to tip variance: \n" + str(var_process) + "\n\n")
 
     firstTime = False
